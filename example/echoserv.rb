aa2e700e447fff4d776e4fbaeb9bf0dec238cb45
skliew <skliew@gmail.com>
2014-08-30 00:52:43 +0800

    Fix example/echoserv.rb

--- example/echoserv.rb
+++ example/echoserv.rb
@@ -1,22 +1,19 @@
 require 'dyad'
 
-on_error = lambda { |event| puts event.msg }
-on_data = lambda do |event|
-  event.stream.write(event.data, event.length)
-  puts event.stream.get_bytes_sent
-end
-on_accept = lambda do |event|
-  event.remote.add_listener(:EVENT_DATA, on_data, nil)
+Dyad.init
+stream = Dyad::Stream.new
+stream.add_listener(:EVENT_ERROR) {|event| puts event.msg }
+
+stream.add_listener(:EVENT_ACCEPT) do |event|
+  event.remote.add_listener(:EVENT_DATA) do |data_evt|
+    data_evt.stream.write(data_evt.data, data_evt.length)
+  end
   event.remote.write("echo server\r\n")
 end
 
-Dyad.init
-stream = Dyad::Stream.new
-stream.add_listener(:EVENT_ERROR, on_error, nil)
-stream.add_listener(:EVENT_ACCEPT, on_accept, nil)
-
 stream.listen(8000)
 
-while Dyad.get_stream_count
+while Dyad.get_stream_count > 0
   Dyad.update
 end
+
